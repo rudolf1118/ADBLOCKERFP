ba84ba75d5ba021b794aa5e67fd7ecb926414289
rudolf1118 <rud.haroutyunyan@gmail.com>
2022-04-13 19:00:13 +0400

    ADBLOCKER UPDATE

--- script.js
+++ script.js
@@ -1,59 +1,31 @@
-
-let sponsoredTexts=[
-    "Sponsored",
-    "مُموَّل", // Arabic
-    "赞助内容", // Chinese (Simplified)
-    "贊助", // Chinese (Traditional)
-    "Sponzorováno", // Czech
-    "Gesponsord", // Dutch
-    "May Sponsor", // Filipino
-    "Commandité", // French (Canada)
-    "Sponsorisé", // French
-    "Gesponsert", // German
-    "Χορηγούμενη", // Greek
-    "ממומן", // Hebrew
-    "प्रायोजित", // Hindi
-    "Hirdetés", // Hungarian
-    "Bersponsor", // Indonesian
-    "Sponsorizzato", // Italian
-    "広告", // Japanese
-    "Sponsorowane", // Polish
-    "Patrocinado", // Portuguese (Brazil)
-    "Sponsorizat", // Romanian
-    "Реклама", // Russian
-    "Sponzorované", // Slovak
-    "Publicidad", // Spanish
-    "Sponsrad", // Swedish
-    "ได้รับการสนับสนุน", // Thai
-    "Sponsorlu", // Turkish
-    "Được tài trợ", // Vietnamese
-];
-let sponsores=sponsoredTexts.map((element)=>`a[aria - label="${element}"]`)
-let blockList=["._m8c", ".uiStreamSponsoredLink", 'a[data-hovercard][href*="hc_ref=ADS"]', 'a[role="button"][rel~="noopener"][data-lynx-mode="async"]',];
+import {blockList, sponsores} from "./other";
 
 let list=[...sponsores, ...blockList];
 
 let blocker=()=>{
-    document.querySelectorAll('[role="feed"] [data-pagelet]').forEach(element => {
-        for(let i = 0;i<list.length;i++) {
-        let spelement = element.querySelector(`[aria-label="${list[i]}"]`)
-        if (spelement !== null) element.style.display = 'none'
-    }}) };
+    document.querySelectorAll('[role="feed"] [data-pagelet]').forEach(element=>{
+        for (let i=0; i < list.length; i++) {
+            let spelement=element.querySelector(`[aria-label="${list[i]}"]`)
+            if (spelement !== null) element.style.display='none'
+        }
+    })
+};
 
-let RemoveFromFeed=() =>{
-    let d = document,
-        postFeed = d.querySelector('[role="feed"]'),
-        postFeedObserver = new MutationObserver(
-            mutations => mutations && blocker()
+let RemoveFromFeed=()=>{
+    let d=document,
+        postFeed=d.querySelector('[role="feed"]'),
+        postFeedObserver=new MutationObserver(
+            mutations=>mutations && blocker()
         )
 
     postFeed &&
     postFeedObserver.observe(postFeed, {
-        childList: true
+        childList: true,
+        subtree: true
     })
-    };
+};
 
 setInterval(function () {
     blocker();
-    RemoveFromFeed();
-}, 100);
+    RemoveFromFeed()
+}, 1000000);
